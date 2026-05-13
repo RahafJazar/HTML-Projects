89b8d0c8df9fce2bb35f051fd73d4cec1535347f
RahafJazar <rahafjazar73@gmail.com>
2026-05-13 16:05:02 +0300

    js project practice

--- Javascript/Document/JS Fundementals/Js/main.js
+++ Javascript/Document/JS Fundementals/Js/main.js
@@ -7,5 +7,94 @@
 // const age = someCode(birthday);
 
 'use strict'
-var name = prompt("What is your name ?", "");
-alert(`The name is ${name}`);
+// var name = prompt("What is your name ?", "");
+// alert(`The name is ${name}`);
+
+
+let x = 1;
+x = -x;
+alert(x);
+
+//+ unary plus =>one operand
+
+//+ binary plus  => Two operands
+
+
+// function printTotalAges() {
+//     let age1 = document.getElementById("user1Age").value;
+//     console.log('type of age1', typeof (+age1)); //unary plus
+//     let age2 = document.getElementById("user2Age").value;
+//     console.log('type of age2', typeof (+age2)); //unary plus
+//     console.log(age1 + age2); //string+string =string 
+//     console.log(+age1 + age2); //+string + string =number +string = string 
+//     console.log(+age1 + +age2);  //+string + +string =number +number =number
+// }
+/**
+ * @content => chained assignment & binary and unary opeartors
+ */
+/*
+let c, f, j, k;
+c = f = j = k = 5; //chained assignment->not prefer to use it  ->better is to split them in lines ;
+console.log('' + c + f + j + k); //binary plus 
+ */
+
+/**
+ * @content =>bitwise operators
+ */
+//bitwise operators 
+//& AND 
+console.log(10 & 3); //2 (true)
+console.log(5 & 0); //0 (false);
+console.log(4 & ''); //0(false);
+console.log(6 & '7'); //6(true);
+console.log(9 & null); //0(false);
+console.log(7 & true); //1(true);
+console.log(false & true); //0(fslse);
+console.log(true & undefined); //0(false);
+console.log(9 & NaN); //0(false);
+//| OR 
+console.log("**********************");
+console.log(10 | 3); //2 (true)
+console.log(5 | 0); //  (true);
+console.log(4 | ''); // (true);
+console.log(6 | '7'); //(true);
+console.log(9 | null); //(true);
+console.log(7 | true); //(true);
+console.log(false | true); //(true);
+console.log(true | undefined); //(true);
+console.log(9 | NaN); //(true);
+
+//^ XOR 
+console.log("**********************");
+console.log(1 ^ 3); //0001 ^ 0011 = 0010;
+
+//~ NOT
+console.log("**********************");
+console.log(~2); // ~0010 = 1101  //-3
+
+//<< LIFT SHIFT 
+console.log("**********************");
+console.log(5 << 1); //00000101 <<1 = 00001010=10
+
+//>> RIGHT SHIFT 
+console.log("**********************");
+console.log(5 >> 1); //00000101 <<1 = 00000010 =2
+
+//>>> Zero Fill RIGHT SHIFT 
+console.log("**********************");
+console.log(5 >> 1); //00000101 <<1 = 00000010 =2
+
+//tasks
+let a, b;
+a = b = 1; //chained assignment 
+let c = ++a; //prefix increment 
+let d = b++; //postfix increment 
+console.log("**********************");
+console.log(c);//2
+console.log(d); //1
+
+
+console.log("**********************");
+console.log("" + 1 + 0);//10
+console.log("" - 1 + 0); // -1
+console.log(true + false)
--- Javascript/session3/project/js/main.js
+++ Javascript/session3/project/js/main.js
@@ -9,7 +9,10 @@ var ProdPrice = window.document.getElementById("prodPrice");
 var prodImg = window.document.getElementById("prodImg");
 var prodCategory = window.document.getElementById("prodCategory");
 var prodDesc = window.document.getElementById("prodDesc");
-
+var searchInput = window.document.getElementById("searchInput");
+var addBtn = document.getElementById("add-btn");
+var updateBtn = document.getElementById("update-btn");
+var currentIndex;
 // console.log(prodName);
 // console.log(ProdPrice);
 // console.log(prodImg);
@@ -72,7 +75,7 @@ function displayProducts() {
                         </div>
                        
                             <button class="btn btn-danger w-100 my-2" onclick="deleteProduct(${i})"> Delete Product</button>
-                            <button class="btn btn-warning w-100 my-2"> Update Product</button>
+                            <button class="btn btn-warning w-100 my-2" onclick="setFormForUpdate(${i})"> Update Product</button>
                    
                     </div>
                
@@ -85,4 +88,67 @@ function displayProducts() {
 
     }
     document.getElementById("myRow").innerHTML = productDiv;
+}
+
+
+function searchProduct() {
+    var term = searchInput.value;
+    var cartona = '';
+    for (var i = 0; i < allProducts.length; i++) {
+        if (allProducts[i].name.toLowerCase().includes(term.toLowerCase())) {
+
+            console.log("yes");
+            cartona += `
+            <div class="col-md-3 p-3">
+                    <div class="bg-dark">
+                    <p class="text-white">${i}</p>
+                        <img src="./images/logo-5-DFypfU0k.jpg" alt="" class="w-100">
+                        <h2 class="h5 text-white">${allProducts[i].name.replace(term, `<span class="bg-warning"> ${term} </span>`)}</h2>
+                        <div class="d-flex justify-content-between">
+                            <span class="text-success">${allProducts[i].price} JD </span>
+                            <span class="text-warning">${allProducts[i].category}</span>
+                        </div>
+                       
+                            <button class="btn btn-danger w-100 my-2" onclick="deleteProduct(${i})"> Delete Product</button>
+                            <button class="btn btn-warning w-100 my-2" onclick="setFormForUpdate(${i})"> Update Product</button>
+                   
+                    </div>
+               
+                    
+                </div>
+            
+            `
+        }
+    }
+    document.getElementById("myRow").innerHTML = cartona;
+}
+
+
+function setFormForUpdate(index) {
+    var product = allProducts[index]
+    prodName.value = product.name;
+    ProdPrice.value = product.price;
+    prodCategory.value = product.category;
+    prodDesc.value = product.desc;
+    addBtn.classList.add("d-none");
+    updateBtn.classList.remove("d-none");
+    currentIndex = index;
+}
+
+function updateProduct() {
+    //create object to describe product 
+    var product = {
+        name: prodName.value,
+        price: ProdPrice.value,
+        src: 'images/fdf',
+        category: prodCategory.value,
+        desc: prodDesc.value
+    }
+    allProducts.splice(currentIndex, 1, product);
+    displayProducts();
+    localStorage.setItem("productContainer", JSON.stringify(allProducts));
+    ResetInputs();
+    addBtn.classList.remove("d-none");
+    updateBtn.classList.add("d-none");
+
 }
