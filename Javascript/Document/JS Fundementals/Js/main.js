84e0d9d26acd8b208daa667f4d349e23b3ef418a
Rahaf Jazar <rahafjazar73@gmail.com>
2026-05-13 06:32:13 +0300

    js document 13-5

--- Javascript/Document/JS Fundementals/Js/main.js
+++ Javascript/Document/JS Fundementals/Js/main.js
@@ -1,7 +1,11 @@
-//variables//
-let name = "John";
-let admin = name;
-alert(admin);
+// //variables//
+// let name = "John";
+// let admin = name;
+// alert(admin);
 
-const BIRTHDAY = '2001-03-01';
-const age = someCode(birthday);
+// const BIRTHDAY = '2001-03-01';
+// const age = someCode(birthday);
+
+'use strict'
+var name = prompt("What is your name ?", "");
+alert(`The name is ${name}`);
