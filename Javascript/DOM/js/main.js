80e707ed93f810fc947b77fca33729d9a1cd1e79
Rahaf Jazar <rahafjazar73@gmail.com>
2026-06-14 06:50:53 +0300

    uu14/6/2026

--- Javascript/DOM/js/main.js
+++ Javascript/DOM/js/main.js
@@ -1,57 +1,97 @@
-//
-var allBtns = document.querySelectorAll('button'); //return NodeList
-console.log(allBtns);
+// //
+// var allBtns = document.querySelectorAll('button'); //return NodeList
+// console.log(allBtns);
 
-for (var i = 0; i < allBtns.length; i++) {
-
-
-    allBtns[i].addEventListener('click', function (e) {
-        //anonymous function
-
-        printWelcome();
-        changeButtonName(e);
-    })
-}
-
-function printWelcome() {
-    console.log('welcome!')
-}
-
-function changeButtonName(element) {
-    element.target.innerHTML = 'Reset';
-}
+// for (var i = 0; i < allBtns.length; i++) {
 
 
-var allImgs = document.querySelectorAll('img');
-var myImg = document.getElementById('myImg');
+//     allBtns[i].addEventListener('click', function (e) {
+//         //anonymous function
 
-for (var i = 0; i < allImgs.length; i++) {
+//         printWelcome();
+//         changeButtonName(e);
+//     })
+// }
 
-    allImgs[i].addEventListener('click', function (e) {
-        console.log(e.target);
-        var srcImg = e.target.getAttribute('src');
-        myImg.setAttribute('src', srcImg);
+// function printWelcome() {
+//     console.log('welcome!')
+// }
 
-
-    })
-
-    allImgs[i].addEventListener('mousedown', function (e) {
-        setBorder(e);
-
-    })
-    allImgs[i].addEventListener('mouseup', function (e) {
-        ResetBorder(e);
-
-    })
-}
-
-function setBorder(element) {
-
-    element.target.style = "border : 2px solid red";
-}
-function ResetBorder(element) {
-
-    element.target.style = "border :0px";
-}
+// function changeButtonName(element) {
+//     element.target.innerHTML = 'Reset';
+// }
 
 
+// var allImgs = document.querySelectorAll('img');
+// var myImg = document.getElementById('myImg');
+
+// for (var i = 0; i < allImgs.length; i++) {
+
+//     allImgs[i].addEventListener('click', function (e) {
+//         console.log(e.target);
+//         var srcImg = e.target.getAttribute('src');
+//         myImg.setAttribute('src', srcImg);
+
+
+//     })
+
+//     allImgs[i].addEventListener('mousedown', function (e) {
+//         setBorder(e);
+
+//     })
+//     allImgs[i].addEventListener('mouseup', function (e) {
+//         ResetBorder(e);
+
+//     })
+// }
+
+// function setBorder(element) {
+
+//     element.target.style = "border : 2px solid red";
+// }
+// function ResetBorder(element) {
+
+//     element.target.style = "border :0px";
+// }
+
+var helloH2 = document.getElementById("hello");
+console.log(helloH2)
+helloH2.style.background = "green"
+
+var h2HTMLColl = document.getElementsByTagName("h2")
+var h2Arr = Array.from(h2HTMLColl);
+console.log(h2HTMLColl)
+
+console.log("************************************");
+console.log(h2Arr)
+console.log("****************************************");
+console.log(typeof (h2HTMLColl));
+console.log(typeof (h2Arr));
+
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+var pByClass = document.getElementsByClassName("test");
+console.log(pByClass)
+console.log(typeof (pByClass));
+
+console.log("=======================================");
+var pByAttr = document.getElementsByName("aya");
+console.log(pByAttr);
+console.log(typeof (pByAttr));
+console.log("=======================================");
+var pByquery = document.querySelector(".testquery");
+console.log(pByquery.innerHTML); //
+console.log(pByquery.innerText);
+console.log(pByquery.textContent);
+
+pByquery.innerHTML = "<i> hello </i>";
+pByquery.innerText = "<i> hello </i>";
+pByquery.textContent = "<i> hello </i>";
+
+console.log("=========event inffo==============");
+pByquery.style.cssText = "background-color:blue; color:white ; font-size:20px";
+console.log(window.getComputedStyle(pByquery));
+pByquery.addEventListener("click",
+    function (event) {
+        console.log(event)//pointer event 
+    }
+)
---
+++ Javascript/problems/js/debugging.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    let result = a + b;
+    setTimeout(() => {
+        console.log("Hello");
+    }, 1000);
+}
+console.log(total);
+var total = add(5, 3);
+console.log(total);
